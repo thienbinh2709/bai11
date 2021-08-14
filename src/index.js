08f5013718afd765daf2b21c7cd9450f399630f1
sypho <syphongabc@gmail.com>
2021-08-15 05:56:35 +0700

    test

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.engine(
 app.set('       view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-route(app);
+route(       app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
